cea4cebc2cfea613a99a37b96c4a06205cc95be1
impulse7-tech <krmlk145@gmail.com>
2025-11-07 03:56:40 +0200

    Update models.py

--- app/backend/models.py
+++ app/backend/models.py
@@ -1,4 +1,4 @@
-"from pydantic import BaseModel, Field, EmailStr, ConfigDict
+from pydantic import BaseModel, Field, EmailStr, ConfigDict
 from typing import List, Optional
 from datetime import datetime, timezone
 import uuid
@@ -99,4 +99,3 @@ class Token(BaseModel):
     access_token: str
     token_type: str = \"bearer\"
     user: User
-"
